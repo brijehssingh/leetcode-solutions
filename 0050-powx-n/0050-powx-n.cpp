20b2c5f59c140f0d89e048757ae734e4585f8841
Brijesh Singh <brijeshkumar4705r@gmail.com>
2026-07-06 18:03:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.27%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+
+double solve(double x, long long n) {
+
+        if (n == 0)
+            return 1;
+
+        if (n % 2 == 0)
+            return solve(x * x, n / 2);
+
+        return x * solve(x * x, n / 2);
+    }
+
+    double myPow(double x, int n) {
+  
+             long long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return solve(x, N);
+
+    }
+};
