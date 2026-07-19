68f21e6daa1f736a1eb73cfd5ac2dd35ca831d96
Brijesh Singh <brijeshkumar4705r@gmail.com>
2026-07-19 13:36:32 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.14%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> seen(26, 0);
+       vector<int> last(26, 0);
+
+      
+        for(int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+
+        string ans;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            int val = s[i] - 'a';
+
+           
+            if(seen[val]) {
+                continue;
+            }
+
+            while(!ans.empty() &&
+                s[i] < ans.back() &&last[ans.back() - 'a'] > i ) {
+
+                seen[ans.back() - 'a'] = 0;
+                ans.pop_back();
+            }
+
+            seen[val] = 1;
+            ans.push_back(s[i]);
+        }
+
+        return ans;
+
+    }
+};
