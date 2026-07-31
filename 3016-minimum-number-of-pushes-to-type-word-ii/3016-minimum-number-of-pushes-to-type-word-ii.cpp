a3d2aae33b4a65cae59bca8121116a8431bd6125
Brijesh Singh <brijeshkumar4705r@gmail.com>
2026-08-01 00:10:31 +0530

    Time: 40 ms (16.41%), Space: 26.9 MB (46.22%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+
+
+  static  bool compare(pair<char,int> a , pair<char,int>b){
+
+    return a.second>b.second;
+     
+}
+
+
     int minimumPushes(string word) {
         
 unordered_map<char, int> mp;
@@ -8,20 +17,20 @@ for (char ch : word) {
     mp[ch]++;
 }
 
-vector<int> freq;
+vector<pair<char,int>> freq;
 
 for (auto i : mp) {
-    freq.push_back(i.second);
+    freq.push_back({i.first , i.second});
 }
 
-sort(freq.begin(), freq.end(), greater<int>());
+sort(freq.begin(), freq.end(),compare);
 
 
 int ans = 0;
 
 for (int i = 0; i < freq.size(); i++) {
     int pushes = (i / 8) + 1;
-    ans += freq[i] * pushes;
+    ans += freq[i].second * pushes;
 }
 
 return ans;
